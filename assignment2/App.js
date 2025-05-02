3ee854c391d1c1da29d461800c8192ef9e658cc0
ziqba1 <93111250+ziqba1@users.noreply.github.com>
2025-05-01 21:33:49 -0400

    Add files via upload

---
+++ assignment2/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+const colors = [
+  { name: 'Gold', value: '#FFD700' },
+  { name: 'Silver', value: '#C0C0C0' },
+  { name: 'Crimson', value: '#DC143C' },
+  { name: 'Sapphire Blue', value: '#0F52BA' },
+  { name: 'Emerald Green', value: '#50C878' },
+];
+
+function App() {
+  const [colorIndex, setColorIndex] = useState(0);
+
+  const handleClick = () => {
+    setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+  };
+
+  return (
+    <div
+      className="container"
+      style={{ backgroundColor: colors[colorIndex].value }}
+    >
+      <div className="content">
+        <h1>{colors[colorIndex].name}</h1>
+        <button onClick={handleClick}>Press to Change Color</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
